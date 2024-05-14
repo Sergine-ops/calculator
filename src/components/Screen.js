ee832451cc66dd9d6b7a5f8f3cfc6cc36d35c57c
Sergine-ops <156674288+Sergine-ops@users.noreply.github.com>
2024-05-14 15:24:52 +0200

    calculator

---
+++ src/components/Screen.js
@@ -0,0 +1,15 @@
+import { Textfit } from "react-textfit";
+import './Screen.css';
+
+export default function Screen({ value }) {
+
+    return (
+
+        <Textfit className='screen' mode="single" max={70}>
+            {value}
+        </Textfit>
+
+
+    );
+
+};
